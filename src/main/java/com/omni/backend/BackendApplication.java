6d3eadbacc096b521d50a8369d5cde09e92827cc
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-02-25 23:12:33 -0300

    Adicionando como submodulo

--- src/main/java/com/omni/backend/BackendApplication.java
+++ src/main/java/com/omni/backend/BackendApplication.java
@@ -2,8 +2,14 @@ package com.omni.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.omni.aurora.core.model"})
+@EnableJpaRepositories({"com.omni.aurora.core.repository"})
 public class BackendApplication {
 
     public static void main(String[] args) {
