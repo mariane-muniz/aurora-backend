309b7e3c29ce1f18b4cde15bb7f53851bdf2472f
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-02-26 16:10:49 -0300

    Migrando para endereço de configuraçao na cloud

---
+++ src/main/java/com/omni/backend/controller/BackendTestController.java
@@ -0,0 +1,17 @@
+package com.omni.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class BackendTestController
+{
+    @ResponseBody
+    @GetMapping
+    public String test() {
+        return "Thats work!";
+    }
+}
